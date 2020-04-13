17541f93a6f6adaeb6d0a0860092610d3f363f4c
Lauren Boone <lboone2094@gmail.com>
2020-04-13 10:34:02 -0600

    added Screens folder and teacher dir

---
+++ lib/Screens/Teachers/teacherhome.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+
+class TeacherHome extends StatelessWidget{
+  @override
+
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home")
+      ),
+        );
+  }
+}
